260a1e25c64b40a6e67e6d5dfd45665c0a3a975e
AmaanIrfan2 <amaanirfan2@gmail.com>
2025-03-08 16:11:16 +0530

    Final Code

--- assetv_ico.sol
+++ assetv_ico.sol
@@ -1,6 +1,8 @@
 // AssetV ICO 
 
 // version of the compiler
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity >=0.4.21 <0.9.0;
 
 contract assetv_ico {
@@ -25,12 +27,28 @@ contract assetv_ico {
     }
 
     //Getting the equity of assetv of the investor 
-    function equity_in_assetv(address investor) external constant returns (uint) {
+    function equity_in_assetv(address investor) external view returns (uint) {
         return equity_assetv[investor];
     }
 
     //Getting the equity of USD of the investor 
-    function equity_in_usd(address investor) external constant returns (uint) {
+    function equity_in_usd(address investor) external view returns (uint) {
         return equity_usd[investor];
+    }
+
+    //buy assetv
+    function buy_assetv(address investor, uint usd_invested) external //this is the function
+    check_max(usd_invested) {   //modifier checks whether the investor can buy AssetV without exceeding the total supply 
+        uint assetv_bought= usd_invested*usd_to_assetv; //This line calculates how many AssetV tokens the investor will receive
+        equity_assetv[investor] +=assetv_bought; // Adds the bought tokens to the investor’s existing balance 
+        equity_usd[investor] += usd_invested; //updating investor's total equity 
+        total_assetv_bought += assetv_bought; //updating total assetV bought 
+    }
+
+    //selling assetv
+    function sell_assetv(address investor, uint assetv_sold) external {     
+        equity_assetv[investor] -=assetv_sold; 
+        equity_usd[investor] = equity_assetv[investor] / 1000; 
+        total_assetv_bought -= assetv_sold; 
     }
 }
